0bfbcfdee18d7cc59a12a7da459585d4ab57dc52
Pedroporto87 <pedroporto_8@hotmail.com>
2023-04-08 22:44:13 -0300

    axios post is done

--- vite-project/src/components/postCard.jsx
+++ vite-project/src/components/postCard.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/postCard.scss'
+import axios from 'axios'
 export const PostCard = () => {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    const postData = () => {
+        const username = localStorage.getItem('myUsername')
+        axios.post(`https://dev.codeleap.co.uk/careers/`,{
+            username, 
+            title,
+            content,
+        })
+        
+      }
+
   return (
     <>
     <div className='post-cart'>
@@ -9,13 +23,13 @@ export const PostCard = () => {
         </div>
         
             <label htmlFor='title' className='post-cart-label-1'>Title</label>
-                <input type='text' className='post-cart-text' name='title' required placeholder='Hello World'></input>
+                <input type='text' className='post-cart-text' name='title' required placeholder='Hello World' onChange={(e) => setTitle(e.target.value)}></input>
             
             <label htmlFor='textbox' className='post-cart-label-2'>Content</label>
-                <input type='textbox' name='textbox' className='post-cart-textbox' required placeholder='Content'></input>
+                <input type='textbox' name='textbox' className='post-cart-textbox' required placeholder='Content' onChange={(e) => setContent(e.target.value)}></input>
             
             <div className='post-card-b-conteiner'>
-                <button className='post-cart-button'>Create</button>
+                <button className='post-cart-button' disabled={!title || !content } onClick={postData}>Create</button>
             </div>
         
 
